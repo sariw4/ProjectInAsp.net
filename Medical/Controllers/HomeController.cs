b8564e9e21c4bd4037dbf87c8625782230fc6e6a
Avital-coder <avitalprives@gmail.com>
2020-10-15 00:18:21 +0300

    checks

--- Medical/Controllers/HomeController.cs
+++ Medical/Controllers/HomeController.cs
@@ -68,6 +68,11 @@ namespace Medical.Controllers
             if (NDCs.GetMedicineByNdc(ndc).Count() == 0) //there is no such NDC
             {
                 ViewBag.message2 = "There is no such a medicine NDC!";
+                ViewBag.CommercialName = collection["CommercialName"];
+                ViewBag.GenericName = collection["GenericName"];
+                ViewBag.Producer = collection["Producer"];
+                ViewBag.ActiveIngredients = collection["ActiveIngredients"];
+                ViewBag.DoseCharacteristic = collection["DoseCharacteristic"];
                 return View();
             }
             
